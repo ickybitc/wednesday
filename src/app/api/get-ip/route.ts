e2190dd8eab34b46e10e4100ea7dfd627a196725
ickybitc <Driftkiller509@gmail.com>
2025-03-27 15:50:30 -0700

    Fix IP address endpoint

--- src/app/api/get-ip/route.ts
+++ src/app/api/get-ip/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const forwardedFor = request.headers.get('x-forwarded-for');
-  const ip = forwardedFor ? forwardedFor.split(',')[0] : request.ip || 'Unknown IP';
+  const ip = forwardedFor ? forwardedFor.split(',')[0] : 'Unknown IP';
   
   return NextResponse.json({ ip });
 } 
